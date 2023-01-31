dbf9a844b20e088be89d1095dfd53bdd72dd07e2
Gabriel Freitas <69334324+gabrielfreitasc@users.noreply.github.com>
2023-01-31 10:40:44 -0300

    Adding animation to pages, and  matunenance
    Adding animation to pages, and responsive matunenance.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,21 @@ menu.addEventListener('click', () => {
     menu.classList.toggle('main-menu');
     navbar.classList.toggle('open');
 })
+
+function reveal() {
+    const reveals = document.querySelectorAll('.reveal');
+
+    reveals.forEach((reveal) => {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveal.getBoundingClientRect().top;
+        const elementVisible = 100;
+
+        if(elementTop < windowHeight - elementVisible) {
+            reveal.classList.add('active');
+        } else {
+            reveal.classList.remove('active');
+        }
+    });
+}
+
+window.addEventListener("scroll", reveal);
