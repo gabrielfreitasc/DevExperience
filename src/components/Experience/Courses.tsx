337cc19e1dc9b53c89225209426c25da671b3854
gabrielfreitasc <gabrielcoelhito2020@gmail.com>
2023-08-22 14:10:25 -0300

    Doc: config animation modal of Courses.tsx for id

--- src/components/Experience/Courses.tsx
+++ src/components/Experience/Courses.tsx
@@ -10,6 +10,8 @@ import { Modal } from "./Modal";
 export function Courses() {
   const [dataCourses, setDataCourses] = useState([]);
   const [openDescription, setOpenDescription] = useState(dataCourses[0]);
+  const [opened, setOpened] = useState(false);
+
   const { t } = useTranslation("global");
 
   useEffect(() => {
@@ -30,7 +32,7 @@ export function Courses() {
           return (
             <div key={id} id={id} className="flex items-center xs:w-full xs:flex-col xs:items-start">
               <div className="flex gap-4 w-1/2 ml-20 xs:w-auto xs:ml-8">
-                <motion.button onClick={() => setOpenDescription(id)} transition={{duration: 0.3}} className={`group inset-0 w-12 h-12 p-2 border-2 border-violet-800 rounded-full bg-transparent flex items-center bg-gradient-to-r hover:bg-gradient-to-r from-violet-800 to-purple-950 cursor-pointer ease-in-out duration-500 ${openDescription !== id && "bg-none"}`}>
+                <motion.button onTap={() => setOpened(!opened)} onClick={() => setOpenDescription(id)} transition={{duration: 0.3}} className={`group inset-0 w-12 h-12 p-2 border-2 border-violet-800 rounded-full bg-transparent flex items-center bg-gradient-to-r hover:bg-gradient-to-r from-violet-800 to-purple-950 cursor-pointer ease-in-out duration-500 ${openDescription !== id && "bg-none"}`}>
                   <PiGraduationCap size={30} className="group-hover:rotate-6 ease-linear duration-500"/>
                 </motion.button>
                 <div className="flex flex-col">
@@ -38,9 +40,9 @@ export function Courses() {
                   <p className="font-sans text-end text-violet-600 xs:text-start">{plataform}</p>
                 </div>
               </div>
-              {openDescription === id && (
+              {openDescription === id && opened === true ? (
                 <Modal openDescription={openDescription} description={description} link={link} idItem={id} skills={skills}/>
-              )}
+              ) : null}
             </div>
           );
         })}
